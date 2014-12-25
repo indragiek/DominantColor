81c0b6379354f8c5a698deda1b162344924ee771
Indragie Karunaratne <i@indragie.com>
2014-12-24 22:22:08 -0700

    Implemented CIE76 and CIE94

---
+++ DominantColor/Shared/INVector3SwiftExtensions.swift
@@ -0,0 +1,14 @@
+//
+//  INVector3SwiftExtensions.swift
+//  DominantColor
+//
+//  Created by Indragie on 12/24/14.
+//  Copyright (c) 2014 Indragie Karunaratne. All rights reserved.
+//
+
+extension INVector3 {
+    func unpack() -> (Float, Float, Float) {
+        return (x, y, z)
+    }
+}
+
